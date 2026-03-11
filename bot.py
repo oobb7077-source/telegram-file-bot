8581ac9ede51fb1f8d1bc23758d569f59740570e
oobb7077-source <oobb7077@gmail.com>
2026-03-11 22:52:11 +0700

    Restrict file uploads to specific owner
    Added OWNER_ID check to restrict file uploads.

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
 import telebot
 
 TOKEN ="8654488281:AAFeBq9-2Gang_0r3bM7P6bBJEuV-a7hPEk"
-
+OWNER_ID = 6495482801
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Send me a file.")
+    bot.send_message(message.chat.id, "🚀 Welcome to Turbo File Bot\nSend me a file!")
 
-@bot.message_handler(content_types=['document','video','audio'])
+@bot.message_handler(content_types=['document'])
 def file_handler(message):
-    bot.reply_to(message, "File received!")
+
+    if message.from_user.id != OWNER_ID:
+        bot.send_message(message.chat.id,"❌ You are not allowed to upload files.")
+        return
+
+    bot.send_message(message.chat.id,"✅ File received!")
 
 bot.infinity_polling()
